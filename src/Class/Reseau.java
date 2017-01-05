96a2cec76c8a4e1a9d33e68b4adb33c3b5ec564c
AlexMAt <mateo.alexis82@gmail.com>
2017-01-05 23:10:19 +0100

    Classe réseau modifier

--- src/Class/Reseau.java
+++ src/Class/Reseau.java
@@ -15,74 +15,171 @@ import java.net.UnknownHostException;
  *
  * @author Alexis
  */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+
 public class Reseau {
-    
-    private Capteur capteur;
-    private Adresse adresse;
-    
-    public Reseau (Capteur capteur, Adresse adress){
-        this.capteur = capteur;
-        this.adresse = adresse;
-}
-    
-    public void Connexion()throws UnknownHostException, IOException
-    {
-         
-            String testCo = null;
-   
-        if(capteur instanceof CapteurInterieur){
-           CapteurInterieur captInt = (CapteurInterieur) this.capteur;
-           testCo = "ConnexionCapteur;"+captInt.getIdentifant()+";"+captInt.getType()+";"+captInt.getLocalisation();//ConnexionCapteur;IdentifiantCapteur;TypeCapteur;Bâtiment;Etage;Salle;PositionRelative
-           
-        }else{
-            CapteurExterieur captExt = (CapteurExterieur) this.capteur;
-            testCo = "ConnexionCapteur"+captExt.getIdentifant()+";"+captExt.getType()+";"+captExt.getLocalisation();//ConnexionCapteur;Id;type;coordLat;coordLong
-             
-        }  
-       
-          try{
-                //System.out.println("Port : " +adresse.getPort());
-                Socket client = new Socket(InetAddress.getLocalHost(),7888);
-                System.out.println("Demande de connexion");
-                
-                OutputStream co =  client.getOutputStream();
-                 
-                for (char c : testCo.toCharArray())
-                     co.write(c);
-                client.close();
-          }catch(UnknownHostException e){
-                 e.printStackTrace();
-          }     
-    }
-    public void TransmissionDonnee() throws UnknownHostException, IOException{
-        String dataM = new String("ValeurCapteur;"+capteur.getVal());
+	Socket socket;
+  	BufferedReader in;
+  	PrintWriter out;
+  	String msg;
+        /*@Méthode pour connecter une interface de visualisation au serveur
         
-        try{
-           Socket data = new Socket(adresse.getIp(),adresse.getPort());
-           System.out.println("Envoie de donnée");
-           
-            OutputStream dat =  data.getOutputStream();
-                 
-                for (char c : dataM.toCharArray())
-                     dat.write(c);
-           data.close();
-        }catch(UnknownHostException e){
-            e.printStackTrace();
-        }
-    }
-    public void Deconnexion() throws UnknownHostException, IOException{
-         String testDeco = new String("DeconnexionCapteur;"+capteur.getIdentifant());//DeconnexionCapteur;IdentifiantCapteur
-         
-         try{
-             Socket  client =  new Socket(InetAddress.getLocalHost(),7888);
-             System.out.println("Demande de déconnexion");
-             
-             OutputStream deco =  client.getOutputStream();
-              for (char c : testDeco.toCharArray())
-                     deco.write(c);
-                client.close();
-         }catch(UnknownHostException e){
-                 e.printStackTrace();
-          } 
-    }
+        */
+	public static void Connexion(Socket socket, BufferedReader in, PrintWriter out, String msg) throws UnknownHostException, IOException {
+		
+	  	msg = "ConnexionVisu;IDENT";//Mettre le getID de l'interface de visualisation
+	  	try{ 
+	  		socket = new Socket(InetAddress.getLocalHost(),7888);
+	  		System.out.println("Demande de connexion");
+	  		out = new PrintWriter(socket.getOutputStream());
+	  		out.println(msg);//On envoie la demande de connexion d'une interface de visualisation
+	  		out.flush();//Vidage du buffer
+	  		
+	  		in = new BufferedReader(new InputStreamReader (socket.getInputStream()));
+	  		String message_distant = in.readLine();
+	  		System.out.println(message_distant);
+	  		if(!message_distant.equals("ConnexionOK")){
+	  			System.out.println("ERREUR");
+	  			/*
+	  			 * @il faut mettre un pop up ici
+	  			 */
+                                                        socket.close();
+	  		}
+	  		
+	  		  		
+	  	}catch(UnknownHostException e){
+	  		e.printStackTrace();
+	  	}catch(IOException e) {
+	  		e.printStackTrace();
+	  	}
+	}
+	
+	/*@Méthode pour deconnecter une interface de visualisation du serveur.
+        */
+	public static void Deconnexion(Socket socket, BufferedReader in, PrintWriter out, String msg) throws UnknownHostException, IOException {
+	
+	    msg = "DeconnexionVisu";
+	  	try{ 
+	  		socket = new Socket(InetAddress.getLocalHost(),7888);//getIp et getPort ici
+	  		System.out.println("Demande de Deconnexion");
+	  		out = new PrintWriter(socket.getOutputStream());
+	  		out.println(msg);//On envoie la demande de deconnexion d'une interface de visualisation
+	  		out.flush();//Vidage du buffer
+	  		in = new BufferedReader(new InputStreamReader (socket.getInputStream()));
+	  		String message_distant = in.readLine();
+	  		System.out.println(message_distant);	  	
+	  		if(!message_distant.equals("DeconnexionOK")){
+	  			System.out.println("ERREUR");
+	  			/*
+	  			 * @il faut mettre un pop up ici
+	  			 */
+	  		}
+	  		
+	  		socket.close();
+	  		
+	  	}catch(UnknownHostException e){
+	  		e.printStackTrace();
+	  	}catch(IOException e) {
+	  		e.printStackTrace();
+	  	}
+	}
+	
+        /*@Méthode permettant d'inscrire les capteurs sélection via l'interface à l'interface de visu (Tableau)
+        
+        */
+	public static void InscriptionVisu(Socket socket,BufferedReader in, PrintWriter out, String msg) throws IOException{
+	
+	 	msg = "InscriptionCapteur;Id1";//Remplacer ID par l'id des capteurs sélectionné
+		
+	 	System.out.println("Inscription interface de visu aux capteur Id1");
+	 	out = new PrintWriter(socket.getOutputStream());
+	 	out.println(msg);
+	 	out.flush();
+	 	
+	 	in = new BufferedReader(new InputStreamReader (socket.getInputStream()));
+		String message_distant = in.readLine();
+  		System.out.println(message_distant);	  
+	 	/*
+	 	 * @il faudrat compter le nombre de capteurs que l'on inscrip pour pouvoir Recupéré leur données.
+	 	 * Pour un capteur intérieur :
+	 	 * CapteurPresent;<IdentifiantDuCapteur>;<TypeDuCapteur>;<Bâtiment>;<Etage>;<Salle>;<PositionRelative>
+		 * Pour un capteur extérieur:
+		 * CapteurPresent;<IdentifiantDuCapteur>;<TypeDuCapteur>;<CoordonnéeGPS_Lat>;<CoordonnéeGPS_Long>
+	 	 */
+  		if(message_distant.equals("InscriptionCapteurKO;Id1")){
+  			System.out.println("impossible de valider l’inscription de capteurs");
+  			/*
+  			 * @il faut mettre un pop up ici
+  			 */
+  		}
+	}
+	
+	/*@Méthode permettant de Déconnecter un ou plusieurs capteurs de l'interface de visu
+        
+        */
+	public static void DesinscriptionVisu(Socket socket,BufferedReader in, PrintWriter out, String msg) throws IOException{
+		msg = "DesinscriptionCapteur;Id1";
+		System.out.println("Desinscription interface de visu aux capteur Id1");
+	 	out = new PrintWriter(socket.getOutputStream());
+	 	out.println(msg);
+	 	out.flush();
+	 	
+	 	in = new BufferedReader(new InputStreamReader (socket.getInputStream()));
+		String message_distant = in.readLine();
+  		System.out.println(message_distant);	  
+	 	
+  		if(message_distant.equals("DesinscriptionCapteurKO;Id1")){
+  			System.out.println("impossible de valider la desinscription de capteurs");
+  			/*
+  			 * @il faut mettre un pop up ici
+  			 */
+  		}
+		
+	}
+
+/*@Il manque la méthod de transmission des valeur depuis le serveur vers l'interface afin de remplir le tableau
+        
+        */
+
+	public static void main() {
+	/*	// TODO Auto-generated method stub
+		System.out.println("On ce co");
+	 	try {
+			Connexion(socket,in,out,msg);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	 	System.out.println("Voulez vous deco?");
+	 	@SuppressWarnings("resource")
+		Scanner sc = new Scanner (System.in);
+	 	String clavier = sc.nextLine();
+	 	if(clavier.equals("oui")){
+	 		try {
+				Deconnexion(socket,in,out,msg);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	 	}*/
+		
+	}
+	/*
+	 * @On doit check tout le temps si on reçoit une Deconnexion de capteur d'interface de simulation
+	 * et le géré du côter visualisation (le retiré de la liste des capteurs)
+	 * Le msg reçu étant le suivant "CapteurDeco;<IdentifiantDuCapteur>";
+	 * La mise à jour des données sera reçu grâce au msg suivant
+	 * ValeurCapteur;<IdentifiantDuCapteur>;<ValeurDuCapteur>
+
+	 */
+
+
 }
